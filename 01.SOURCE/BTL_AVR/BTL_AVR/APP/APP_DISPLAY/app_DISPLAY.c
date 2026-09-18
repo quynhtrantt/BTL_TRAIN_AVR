4e211dc1656ce3b27058ec5289952013c94e8a04
Tran Quynh <mookaombre@gmail.com>
2026-09-18 12:27:41 +0700

    change_folderfile

---
+++ 01.SOURCE/BTL_AVR/BTL_AVR/APP/APP_DISPLAY/app_DISPLAY.c
@@ -0,0 +1,6 @@
+/*
+ * app_DISPLAY.c
+ *
+ * Created: 9/18/2026 12:10:37 PM
+ *  Author: Quynh
+ */ 
---
+++ 01.SOURCE/BTL_AVR/BTL_AVR/APP/APP_DISPLAY/app_DISPLAY.h
@@ -0,0 +1,16 @@
+/*
+ * app_DISPLAY.h
+ *
+ * Created: 9/18/2026 12:12:40 PM
+ *  Author: Quynh
+ */ 
+
+
+#ifndef APP_DISPLAY_H_
+#define APP_DISPLAY_H_
+
+
+
+
+
+#endif /* APP_DISPLAY_H_ */
