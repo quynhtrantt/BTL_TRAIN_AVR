96c7c33b960e79b6c37e7a24605ee80a04e01d30
Tran Quynh <mookaombre@gmail.com>
2026-09-25 14:32:18 +0700

    Fix: Remove ignored Debug and build files

---
+++ 01.SOURCE/BTL_AVR/BTL_AVR/Debug/APP/APP_DHT.d
@@ -0,0 +1,18 @@
+APP/APP_DHT.d APP/APP_DHT.o: ../APP/APP_DHT.c ../APP/APP_DHT.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../BSP/BSP_DHT.h ../DATA/DATA_DHT.h ../BSP/BSP_Timer.h ../BSP/BOARD.h
+
+../APP/APP_DHT.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../BSP/BSP_DHT.h:
+
+../DATA/DATA_DHT.h:
+
+../BSP/BSP_Timer.h:
+
+../BSP/BOARD.h:
---
+++ 01.SOURCE/BTL_AVR/BTL_AVR/Debug/DATA/DATA_DHT.d
@@ -0,0 +1,12 @@
+DATA/DATA_DHT.d DATA/DATA_DHT.o: ../DATA/DATA_DHT.c ../DATA/DATA_DHT.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../BSP/BOARD.h
+
+../DATA/DATA_DHT.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../BSP/BOARD.h:
